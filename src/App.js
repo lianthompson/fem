a1d1324bd5e5b23a3fc34e8c12d4d50e659d21a6
Lian Thompson <lianythompson@gmail.com>
2019-05-29 14:50:47 -0700

    configure eslint with react

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React from "react";
-import Pet from "./Pet";
 import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked the title");
-  }
   render() {
-    // return React.createElement("div", {}, [
-    //   React.createElement(
-    //     "h1",
-    //     { onClick: this.handleTitleClick },
-    //     "Adopt Me!"
-    //   ),
-    //   React.createElement(Pet, {
-    //     name: "Luna",
-    //     animal: "dog",
-    //     breed: "Havanese"
-    //   }),
-    //   React.createElement(Pet, {
-    //     name: "Satoshi",
-    //     animal: "cat",
-    //     breed: "Black"
-    //   }),
-    //   React.createElement(Pet, {
-    //     name: "Nakamoto",
-    //     animal: "cat",
-    //     breed: "White"
-    //   })
-    // ]);
-
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt me!</h1>
+        <h1>Adopt me!</h1>
         <Pet name="Luna" animal="dog" breed="Havanese" />
         <Pet name="Satoshi" animal="cat" breed="Black" />
         <Pet name="Nakamoto" animal="cat" breed="White" />
--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 const Pet = props => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, props.name),
-  //     React.createElement("h2", {}, props.animal),
-  //     React.createElement("h2", {}, props.breed)
-  //   ]);
-
   return (
     <div>
       <h1>{props.name}</h1>
