fa67726d23d8df10a8010fa74581d78654341cf3
Lian Thompson <lianythompson@gmail.com>
2019-05-29 13:41:56 -0700

    separate pet component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
+import Pet from "./Pet.js";
 import { render } from "react-dom";
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
 
 class App extends React.Component {
   handleTitleClick() {
---
+++ src/Pet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
