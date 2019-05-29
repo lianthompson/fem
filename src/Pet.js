9e681a3791bb5b5bf44172235c105ed541d3a787
Lian Thompson <lianythompson@gmail.com>
2019-05-29 14:28:14 -0700

    change to h2 for component in JSX

--- src/Pet.js
+++ src/Pet.js
@@ -10,8 +10,8 @@ const Pet = props => {
   return (
     <div>
       <h1>{props.name}</h1>
-      <h1>{props.animal}</h1>
-      <h1>{props.breed}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
     </div>
   );
 };
