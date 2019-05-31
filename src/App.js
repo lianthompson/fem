ffd64ed8f9424a56832da4898ff7d38f9f2eb86d
Lian Thompson <lianythompson@gmail.com>
2019-05-30 20:22:06 -0500

    add lifecycle method componentdidmount

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { render } from "react-dom";
 import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" });
+
+    promise.then(console.log, console.error);
+  }
   render() {
     return (
       <div>
