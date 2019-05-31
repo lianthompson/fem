a0e259b084f0e836ce0d955df096139864b187dc
Lian Thompson <lianythompson@gmail.com>
2019-05-30 21:25:55 -0500

    refactor components for adding reach routing

--- src/App.js
+++ src/App.js
@@ -1,67 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import Results from "./Results";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: "full", location: "San Francisco, CA" })
-      .then(data => {
-        let pets;
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-
-        this.setState({
-          pets
-        });
-      });
-  }
   render() {
     return (
       <div>
-        <h1>Adopt me!</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(", ");
-            } else {
-              breed = pet.breeds.breed;
-            }
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <h1>Adopt Me!</h1>
+        <Results />
       </div>
     );
   }
