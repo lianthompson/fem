77d9c1ebde5f733974f7ce4bea154ce70f3eed59
Lian Thompson <lianythompson@gmail.com>
2019-05-30 21:47:41 -0500

    fix router bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Results />
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
