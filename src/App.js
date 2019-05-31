331127af17c554cf58b9a25e29ce73e10095a16e
Lian Thompson <lianythompson@gmail.com>
2019-05-30 21:12:58 -0500

    passing component data using props

--- src/App.js
+++ src/App.js
@@ -56,6 +56,8 @@ class App extends React.Component {
                 animal={pet.animal}
                 name={pet.name}
                 breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
               />
             );
           })}
