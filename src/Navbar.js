6af7214d59173205e4ba462df7e48e5130084841
Lian Thompson <lianythompson@gmail.com>
2019-06-07 15:29:01 -0700

    animation keyframes with emotion

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/core";
 import colors from "./colors";
+
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Dinosaur = styled("span")`
+  display: inline-block;
+  animation: 1s ${Spin} linear infinite;
+`;
 
 const Container = styled("header")`
   background-color: ${colors.dark};
@@ -14,20 +29,15 @@ const NavLink = styled(Link)`
   &:hover {
     text-decoration: underline;
   }
-
-  span {
-    display: inline-block;
-    border: 1px solid red;
-  }
 `;
 
 const Navbar = () => (
   <Container>
     <NavLink to="/">Adopt Me!</NavLink>
     <NavLink to="/search-params">
-      <span aria-label="search" role="img">
+      <Dinosaur aria-label="search" role="img">
         🦕
-      </span>
+      </Dinosaur>
     </NavLink>
   </Container>
 );
