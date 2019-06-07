a4baa8f9d85873fb2f4e77e7b34c28dd1e0c95ea
Lian Thompson <lianythompson@gmail.com>
2019-06-07 15:07:35 -0700

    reusing styles w javascript

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "@emotion/styled";
+import colors from "./colors";
 
 const Container = styled("header")`
-  background-color: #333;
+  background-color: ${colors.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+
+  span {
+    display: inline-block;
+    border: 1px solid red;
+  }
+`;
+
 const Navbar = () => (
   <Container>
-    <Link to="/">Adopt Me!</Link>
-    <Link to="/search-params">
+    <NavLink to="/">Adopt Me!</NavLink>
+    <NavLink to="/search-params">
       <span aria-label="search" role="img">
         🦕
       </span>
-    </Link>
+    </NavLink>
   </Container>
 );
 
